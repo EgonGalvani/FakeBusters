143f1ad81cec1ff974f5f17142de035f252ec719
Egon <egongalvani@libero.it>
2022-04-09 17:57:06 +0200

    updated index to use new utils and types

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,85 @@
 import { Provider } from "@ethersproject/abstract-provider";
-import { BigNumber, Wallet } from "ethers";
+import { BigNumber, Contract, utils, Wallet } from "ethers";
 import { FakeBusters } from "./FakeBusters";
 import { Outcome } from "./types/outcome";
+import {
+  SystemOutcome,
+  fromBigNumber as bigNumberToSystemOutcome,
+} from "./types/systemOutcome";
 import { Vote } from "./types/vote";
 import { parseVotes } from "./utils/csv";
 import { getProvider } from "./utils/eth";
 
 require("dotenv").config();
 
-// let keys = require("fs").readFileSync("secrets.txt", "utf-8").split(/\r?\n/);
-
-const news: Array<string> = [""];
-
 // correct evaluation of each piece of news
+// TODO: fill this map
 const newsRealEvaluation: Map<string, Outcome> = new Map([
-  // [newsUrl, evaluation], 
+  // [newsUrl, evaluation],
 ]);
 
-// evaluations given by the system
-// null = NO_DECISION
-type SystemOutcome = Outcome | null;
-const systemEvaluation: Map<string, SystemOutcome> = new Map([]);
-
-// map: news url => list of votes for that news
-const votes: Map<string, Array<Vote>> = new Map([]);
-// TODO: parse of form results to fill this map
-
-const pollCreatedHandler = (id: BigNumber, submitter: string, url: string) => {
-  console.log(`[PollCreated] ${id} ${submitter} ${url}`);
-};
-
-const pollClosedHandler = (
-  id: BigNumber,
-  gameOutcome: BigNumber,
-  votingOutcome: BigNumber,
-  certOutcome: BigNumber
-) => {
-  console.log(
-    `[PollClosed] ${id} ${gameOutcome} ${votingOutcome} ${certOutcome}`
-  );
-};
+// TODO: fill with the news srcs
+const news: Array<string> = [];
 
 const init = async () => {
-	await parseVotes("data/raw/answer.csv", "wallet_private_keys.txt", newsRealEvaluation, votes);
+  // id -> news url
+  let urlIds: Map<BigNumber, string> = new Map([]);
 
   // provider used to connect to the considered network
-  const provider: Provider = getProvider();
+  let provider: Provider = getProvider();
+
+  // evaluations given by the system
+  let systemEvaluation: Map<string, SystemOutcome> = new Map([]);
+
+  // map: news url => [vote1, vote2, ...voteN]
+  const votes: Map<string, Array<Vote>> = await parseVotes(
+    "data/raw/answer.csv"
+  );
 
   // object used to connect to the smart contract
   // if no address is passed, a new contract is created
   const contract: FakeBusters = await FakeBusters.build(provider);
+
+  const pollCreatedHandler = (
+    id: BigNumber,
+    submitter: string,
+    url: string
+  ) => {
+    console.log(`[PollCreated] ${id} ${submitter} ${url}`);
+
+    urlIds.set(id, url);
+  };
+
+  const pollClosedHandler = (
+    id: BigNumber,
+    gameOutcome: BigNumber,
+    votingOutcome: BigNumber,
+    certOutcome: BigNumber
+  ) => {
+    console.log(
+      `[PollClosed] ${id} ${gameOutcome} ${votingOutcome} ${certOutcome}`
+    );
+
+    // url of the current news
+    const currentNews: string = urlIds.get(id)!;
+
+    // add system evaluation of the current piece of news to the systemEvaluation map
+    systemEvaluation.set(currentNews, bigNumberToSystemOutcome(gameOutcome));
+
+    votes.get(currentNews)?.forEach(async (vote: Vote) => {
+      /* withdraw only if one of the following statements is true: 
+      - the considered piece of news is evaluated as null (NO_DECISION) by the system 
+      - the user has voted correctly
+    */
+      if (
+        systemEvaluation.get(currentNews) == null ||
+        vote.answer == systemEvaluation.get(currentNews)
+      ) {
+        const voter = new Wallet(vote.account, provider);
+        const withdrawResult = await contract.withdraw(voter, id);
+      }
+    });
+  };
 
   // submitter and expert wallets
   const submitter = new Wallet(process.env.SUBMITTER_PRIVATE_KEY!, provider);
@@ -87,21 +117,6 @@ const init = async () => {
       const voteResult = await contract.vote(voter, vote.answer);
     });
     // ===== VOTING FOR THE CURRENT PIECE OF NEWS ENDS =====
-
-    // 4. withdraw 
-    currentVotes.forEach(async (vote: Vote) => {
-      /* withdraw only if one of the following statements is true: 
-        - the considered piece of news is evaluated as null (NO_DECISION) by the system 
-        - the user has voted correctly
-      */
-      if (
-        systemEvaluation.get(currentNews) == null ||
-        vote.answer == systemEvaluation.get(currentNews)
-      ) {
-        const voter = new Wallet(vote.account, provider);
-        const withdrawResult = await contract.withdraw(voter, newsId);
-      }
-    });
   });
 };
 
